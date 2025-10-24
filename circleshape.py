4ec744e5fc6b8bcbab3e80fb374e6c8e00d0f9b7
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2025-10-24 07:25:51 +0100

    Update circleshape.py

--- circleshape.py
+++ circleshape.py
@@ -2,7 +2,6 @@ import pygame
 
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
-        # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
@@ -13,7 +12,6 @@ class CircleShape(pygame.sprite.Sprite):
         self.radius = radius
 
     def draw(self, screen):
-        # sub-classes must override
         pass
 
     def update(self, dt):
